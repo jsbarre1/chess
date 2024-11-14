7c5c0d2e7a8fae9255aed9d5b1abdcb8c0efeb85
John Barrett <john.bearett@gmail.com>
2024-11-13 17:15:43 -0700

    chess facade login and register

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,44 @@
 package client;
 
+import dataaccess.*;
+import exceptions.ResponseException;
+import model.AuthData;
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
+import service.Services;
 
 
 public class ServerFacadeTests {
-
+    UserData testUser = new UserData("username", "password", "email");
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+    private UserDAO userDAO;
+    private Services services;
     private static Server server;
+    private static ServerFacade serverFacade;
+    private AuthData loggedIn;
+    private String testAuthToken;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        var port = server.run(8080);
         System.out.println("Started test HTTP server on " + port);
+        serverFacade = new ServerFacade("http://localhost:8080");
+    }
+
+    @BeforeEach
+    public void setUp() throws ResponseException, DataAccessException {
+        authDAO = new SQLAuthDAO();
+        gameDAO = new SQLGameDAO();
+        userDAO = new SQLUserDAO();
+        authDAO.deleteAllAuths();
+        userDAO.deleteAllUsers();
+        gameDAO.deleteAllGames();
+        loggedIn = serverFacade.registerUser(testUser);
+        testAuthToken= loggedIn.authToken();
     }
 
     @AfterAll
@@ -22,8 +48,22 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void register() throws ResponseException, DataAccessException {
+        AuthData expected = authDAO.getAuth(loggedIn.authToken());
+        Assertions.assertEquals(expected, loggedIn);
+    }
+
+    @Test
+    public void logout () throws ResponseException, DataAccessException {
+        AuthData authData = new AuthData(null, testAuthToken);
+        serverFacade.logoutUser(authData);
+        Assertions.assertNull(authDAO.getAuth(testAuthToken));
+    }
+    @Test
+    public void login() throws ResponseException, DataAccessException {
+        AuthData actual = serverFacade.loginUser(testUser);
+        AuthData expected = authDAO.getAuth(actual.authToken());
+        Assertions.assertEquals(expected, actual);
     }
 
 }
--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,10 +1,15 @@
 package server;
 
 import com.google.gson.Gson;
+import com.sun.net.httpserver.Request;
 import exceptions.ResponseException;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 
 public class ServerFacade {
 
@@ -13,6 +18,39 @@ public class ServerFacade {
     public ServerFacade(String url) {
         serverUrl = url;
     }
+
+    public void logoutUser(AuthData authToken) throws ResponseException{
+        var path = "/session";
+        this.makeRequest("DELETE", path, authToken, null);
+    }
+
+    public AuthData loginUser(UserData userData) throws ResponseException{
+        var path = "/session";
+        var temp = this.makeRequest("POST", path, userData, AuthData.class);
+        return temp;
+    }
+
+
+    public AuthData registerUser(UserData userData) throws ResponseException{
+        var path = "/user";
+        return this.makeRequest("POST", path, userData, AuthData.class);
+    }
+
+//    private ArrayList<GameData> listGames() throws ResponseException{
+//        var path = "/game";
+//        var hehe = this.makeRequest("GET", path, null, GameData.class);
+//        return new ArrayList<>();
+//    }
+//
+//    public Object joinGame(Request req, Response res) throws ResponseException{
+//        var path = "/game";
+//        return this.makeRequest("PUT", path, pet, Pet.class);
+//    }
+//
+//    public Object createGame(Request req, Response res) throws ResponseException{
+//        var path = "/game";
+//        return this.makeRequest("POST", path, pet, Pet.class);
+//    }
 
     private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
         try {
